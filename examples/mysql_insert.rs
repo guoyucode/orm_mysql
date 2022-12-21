acfcbeff792a5416532ee44834bd4131ae008e20
郭宇 <essence.guo@idreamsky.com>
2022-12-21 16:35:39 +0800

    mysql insert start impl

---
+++ examples/mysql_insert.rs
@@ -0,0 +1,42 @@
+use mysql_async::prelude::{Query, ToConnection, WithParams};
+use orm_uu::{OrmMySql};
+use orm_uu::mysql::ORMr;
+use mysql_async::prelude::Queryable;
+
+#[tokio::main]
+async fn main() -> common_uu::IResult {
+    let ref pool: mysql_async::Pool = mysql_async::Pool::new("mysql://username:pwd@ip:port/db_name");
+    let mut conn: mysql_async::Conn = pool.get_conn().await?;
+    let mut tx = pool.start_transaction(mysql_async::TxOpts::new()).await?;
+    
+    // use connection
+    let sql = "insert into users (user_id, username)values(:user_id, :username)";
+    sql.with(vec![].iter().map(|UserData{ user_id, username }| params! {
+
+    })).batch(&mut conn);
+    // sql: select user_id,username from users where 1 != 1
+    println!("find count: {}", list.len());
+
+    // use transaction
+    let list = UserData::query(&mut tx, "where 1=1", Some(1000)).await?;
+    // sql: select user_id,username from users where 1=1 limit 1000
+    println!("find count: {}", list.len());
+
+    // query_one
+    let one = UserData::query_one(&mut tx, "where 1=1").await?;
+    // sql: select user_id,username from users where 1=1 limit 1
+    println!("find count: {:?}", one.is_some());
+
+    Ok(())
+}
+
+#[derive(OrmMySql)]
+#[orm_mysql(table_name=users)] // is not config: table_name => user_data
+struct UserData {
+    user_id: i64,
+    username: String,
+}
+
+
+
+
