424faac72888c674f928f89b2a894e670ace1312
郭宇 <essence.guo@idreamsky.com>
2022-12-21 11:12:21 +0800

    fmt

--- examples/mysql.rs
+++ examples/mysql.rs
@@ -4,11 +4,15 @@ use orm_uu::mysql::ORMr;
 
 #[tokio::main]
 async fn main() -> common_uu::IResult {
-    let ref pool: mysql_async::Pool =
-        mysql_async::Pool::new("mysql://用户名:密码@ip:端口号/数据库名");
+    let ref pool: mysql_async::Pool = mysql_async::Pool::new("mysql://username:pwd@ip:port/db_name");
     let mut conn: mysql_async::Conn = pool.get_conn().await?;
     let mut tx = pool.start_transaction(mysql_async::TxOpts::new()).await?;
+    
+    // use connection
     let list = User::query(conn, "", None).await?;
+
+    // use transaction
+    let list = User::query(&mut tx, "", None).await?;
     Ok(())
 }
 
