0243975f1e9da5ebfafc5957fefaf8cb71975761
essence <guoyumail@qq.com>
2022-12-26 16:59:54 +0800

    remove scylladb and redis function;

--- macro_lib/lib.rs
+++ macro_lib/lib.rs
@@ -1,7 +1,5 @@
 use proc_macro::TokenStream;
 
-mod redis;
-mod scylla;
 mod mysql;
 mod utils;
 
@@ -15,23 +13,6 @@ pub fn mysql_query(input: TokenStream) -> TokenStream{
     mysql::db_query(input)
 }
 
-
-/// 查询方法
-/// db_query<T: ToString>(session: &Arc<scylla::Session>, where_sql: String, where_in_vars: &Vec<T>, limit_v: Option<isize>) -> R<Vec<Self>>
-#[proc_macro_derive(ScyllaDBQuery)]
-pub fn db_query(input: TokenStream) -> TokenStream{
-    scylla::db_query(input)
-}
-
-#[proc_macro_derive(RedisHget)]
-pub fn cache_query_macro(input: TokenStream) -> TokenStream {
-    redis::cache_query_macro(input)
-}
-
-#[proc_macro_derive(RedisZrange)]
-pub fn cache_query_zrange_macro(input: TokenStream) -> TokenStream{
-    redis::cache_query_zrange_macro(input)
-}
 
 #[test]
 fn test() {}
--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,6 @@
 extern crate serde_json;
 pub use async_trait;
 pub use macro_lib::*;
-pub mod conv_data;
 pub mod mysql;
 pub use mysql_async;
-
-#[cfg(feature="use_scylladb")]
-pub mod scylladb;
-
-#[cfg(feature="use_scylladb")]
-pub use scylla;
 
