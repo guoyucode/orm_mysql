dc90c2dd6e2a2d07783db379717b867f2390e357
essence <guoyumail@qq.com>
2022-12-27 20:56:43 +0800

    doc

--- macro_lib/lib.rs
+++ macro_lib/lib.rs
@@ -3,11 +3,8 @@ use proc_macro::TokenStream;
 mod mysql;
 mod utils;
 
-/* 
-async fn query( comm: C, where_sql: &str, limit: Option<usize>) -> common_uu::IResult<Vec<Self>>
-async fn query_one( comm: C, where_sql: &str) -> common_uu::IResult<Option<Self>>
-*/
-/// 
+
+/// plase see ./examples/xxxx.rs
 #[proc_macro_derive(OrmMySql, attributes(orm_mysql))]
 pub fn mysql_query(input: TokenStream) -> TokenStream{
     mysql::db_query(input)
